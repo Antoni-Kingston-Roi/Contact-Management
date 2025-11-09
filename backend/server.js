6ce74e45d6a3a2d3f757e03371d2f6f8e1f7d807
Antoni-Kingston-Roi <antonikingstonj@gmail.com>
2025-11-10 04:38:32 +0530

    Update frontend components

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,27 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/contacts',contactRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+    })
+}
+
 
 
 mongoose.connect(process.env.MONGO_URI)
--- frontend/src/components/ContactList.jsx
+++ frontend/src/components/ContactList.jsx
@@ -10,7 +10,7 @@ const ContactList = ({ setContacts, contacts }) => {
     const fetchContacts = async ()=>{
       setLoading(true);
       const query = `?status=${filter}&search=${search}`;
-      const fetchPromise = await axios.get(`http://localhost:5000/contacts${query}`)
+      const fetchPromise = await axios.get(`https://contact-management-backend-t2p6.onrender.com${query}`)
       .then((res)=>setContacts(res.data))
       .catch((error)=>console.log(error));
       const delay = new Promise((resolve)=>setTimeout(resolve,1000));
@@ -22,7 +22,7 @@ const ContactList = ({ setContacts, contacts }) => {
 
     const handleStatusChange = async (id,status)=>{
       try {
-        await axios.put(`http://localhost:5000/contacts/${id}`,{status});
+        await axios.put(`https://contact-management-backend-t2p6.onrender.com/${id}`,{status});
         setContacts((prev)=> prev.map((c)=>c._id===id ? {...c,status} :c))
       } catch (error) {
         console.log(error);
@@ -33,7 +33,7 @@ const ContactList = ({ setContacts, contacts }) => {
      const handleDelete = async (id)=>{
       if(confirm("Are you sure you want to Delete ?")){
             try {
-        await axios.delete(`http://localhost:5000/contacts/${id}`);
+        await axios.delete(`https://contact-management-backend-t2p6.onrender.com/${id}`);
         setContacts((prev)=> prev.filter((c)=>c._id !==id ))
       } catch (error) {
         console.log(error);
