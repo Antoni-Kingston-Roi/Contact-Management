7cab9ca4422d6b3f45b0d16c600b836949b5a8b6
Antoni-Kingston-Roi <antonikingstonj@gmail.com>
2025-11-10 05:31:51 +0530

    Update frontend components

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,24 @@ import cors from "cors";
 import dotenv from "dotenv";
 import contactRoutes from "./routes/contactRoutes.js";
 
-
 dotenv.config();
 const app = express();
 
-const PORT = 5000;
-
-
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/contacts',contactRoutes);
+app.use("/contacts", contactRoutes);
 
-
-
-
-mongoose.connect('mongodb+srv://antonikingstonroi_db:0i1Gs1TushuH4hFt@contact-management-syst.5rnqdv4.mongodb.net/?appName=Contact-Management-System')
-.then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://antonikingstonroi_db:0i1Gs1TushuH4hFt@contact-management-syst.5rnqdv4.mongodb.net/?appName=Contact-Management-System"
+  )
+  .then(() => {
     console.log("MONGODB CONNECTED SUCCESSFULLY");
-    app.listen(5000,()=>console.log("Server running on port 5000"))    
-})
-.catch((error)=>console.log(error));
+    app.listen(PORT, () =>
+      console.log(`Server running on port ${PORT}`)
+    );
+  })
+  .catch((error) => console.log(error));
